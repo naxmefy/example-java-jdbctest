9f9b237a6ff549f693fc80c327b18459b30e293b
MRW Neundorf <m.neundorf@live.de>
2018-01-24 17:53:25 +0100

    up

--- src/main/java/me/nax/jdbctest/JDBC.java
+++ src/main/java/me/nax/jdbctest/JDBC.java
@@ -9,9 +9,18 @@ import java.util.Map;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+/**
+ * Before using this class make sure that required JDBC drivers are loaded.
+ */
 public class JDBC {
 	private static final Logger logger = LogManager.getLogger(JDBC.class);
 	
+	/**
+	 * Tests each jdbc url and returns a map with results.
+	 * 
+	 * @param urls Each string should be a jdbc url.
+	 * @return A Map contains urls as keys and a boolean value to indicate the reachability. 
+	 */
 	public static Map<String, Boolean> areReachable(String ...urls) {
 		Map<String, Boolean> results = new HashMap<String, Boolean>();
 		for(String url : urls) {
@@ -20,6 +29,11 @@ public class JDBC {
 		return results;
 	}
 	
+	/**
+	 * Tests a single jdbc url and returns a boolean to indicate the reachability.
+	 * @param url A string that should be a jdbc url.
+	 * @return A boolean value to indicate the reachability. 
+	 */
 	public static boolean isReachable(String url) {
 		try {
 			Connection conn = DriverManager.getConnection(url);
